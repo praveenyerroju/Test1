da5fd71287abc9184452ef71b41b5c8507a9a4fe
praveenyerroju <yerrojupraveenklu@gmail.com>
2021-12-30 23:56:20 +0530

    first program

---
+++ com.test/sample.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class sample {
+    public static void main(String[] args){
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter input value");
+     int i=sc.nextInt();
+     System.out.println("output value is:"+i*5);
+
+    }
+}
